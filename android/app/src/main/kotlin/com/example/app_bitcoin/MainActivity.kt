a8464a6c93e4d32650bf28b0b2def7c808acddc4
unknown <admin@LAB3-27.bglab.local>
2023-10-19 19:41:41 -0300

    criando o projeto e adicionando a classe Bitcoin

---
+++ android/app/src/main/kotlin/com/example/app_bitcoin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_bitcoin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/bitcoin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Bitcoin extends StatefulWidget {
+  const Bitcoin({super.key});
+
+  @override
+  State<Bitcoin> createState() => _BitcoinState();
+}
+
+class _BitcoinState extends State<Bitcoin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
